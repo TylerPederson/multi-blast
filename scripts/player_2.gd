eebd49c2420809f5464973e5f928ecd64fe9907a
Fred Godsey <pithyjam@gmail.com>
2026-01-12 16:00:07 -0800

    Repair missing components
    Fixed missing code
    Fixed missing inputs

--- scripts/player_2.gd
+++ scripts/player_2.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+@export var player_sprite: AnimatedSprite2D
+
 @export var movement_speed = 300
 @export var gravity = 30
 @export var jump_strength = 600
+
+@onready var initial_sprite_scale = player_sprite.scale
 
 func _physics_process(delta: float) -> void:
 	var horizontal_input = (
@@ -13,6 +17,28 @@ func _physics_process(delta: float) -> void:
 	velocity.x = horizontal_input * movement_speed
 	velocity.y += gravity
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	var is_falling = velocity.y > 0.0 and not is_on_floor()
+	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	var is_jump_cancelled = Input.is_action_just_released("jump") and not velocity.y < 0.0
+	var is_idle = is_on_floor() and is_zero_approx(velocity.x)
+	var is_walking = is_on_floor() and not is_zero_approx(velocity.x)
+	
+	if is_jumping:
 		velocity.y = -jump_strength
+	
 	move_and_slide()
+	
+	if is_jumping:
+		player_sprite.play("jump_start")
+	elif is_walking:
+		player_sprite.play("walk")
+	elif is_falling:
+		player_sprite.play("fall")
+	elif is_idle:
+		player_sprite.play("idle")
+	
+	if not is_zero_approx(horizontal_input):
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale
